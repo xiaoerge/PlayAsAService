3723d303839b6d4d05bce5f71515763dc41d5714
QYang <qyang@QYang-MBP.fios-router.home>
2017-01-06 14:55:41 -0500

    dice

---
+++ template.php
@@ -0,0 +1,75 @@
+<?php
+    //Meta data
+    $title = 'PlayAsAService';
+    $description = 'Play as a Service';
+    $author = 'xiaoerge';
+    $keywords = 'Play';
+
+    //Facebook Graph
+    $fb_app_id = '';
+    $fb_admin = '';
+
+    //Open Graph Protocol
+    $og_type = 'website';
+    $og_url = 'https://github.com/xiaoerge/PlayAsAService';
+    $og_description = $description;
+
+    //Twitter Card
+    $twitter_card = 'summary';
+    $twitter_creator = '@';
+    $twitter_title = $title;
+    $twitter_description = $description;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $description; ?>">
+    <meta name="author" content="<?php echo $author; ?>">
+    <meta name="keywords" content="<?php echo $keywords; ?>"/>
+    <meta property="fb:app_id" content="<?php echo $fb_app_id; ?>"/>
+    <meta property="fb:admin" content="<?php echo $fb_admin; ?>"/>
+    <meta property="og:title" content="<?php echo $title; ?>"/>
+    <meta property="og:type" content="<?php echo $og_type; ?>"/>
+    <meta property="og:url" content="<?php echo $og_url; ?>"/>
+    <meta property="og:description" content="<?php echo $og_description; ?>"/>
+    <meta property="og:image" content="./assets/icons/ms-icon-310x310.png"/>
+    <meta property="og:image:width" content="192"/>
+    <meta property="og:image:height" content="192"/>
+    <meta name="twitter:card" content="<?php echo $twitter_card; ?>">
+    <meta name="twitter:creator" content="<?php echo $twitter_creator; ?>">
+    <meta name="twitter:title" content="<?php echo $twitter_title; ?>">
+    <meta name="twitter:description" content="<?php echo $twitter_description; ?>">
+    <meta name="twitter:image" content="./assets/icons/ms-icon-310x310.png">
+    <link rel="apple-touch-icon" sizes="./assets/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="./assets/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./assets/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./assets/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./assets/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./assets/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="./assets/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./assets/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon-16x16.png">
+    <link rel="manifest" href="./assets/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-150x150.png">
+    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-310x310.png">
+    <meta name="theme-color" content="#ffffff">
+    <title><?php echo $title; ?></title>
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+</body>
+</html>
