cd1844a54abc63ef7b114c0d7e9f2cdfe1007225
QYang <qyang@QYang-MBP.fios-router.home>
2017-01-06 15:27:36 -0500

    gif header tryout

--- dice/index.php
+++ dice/index.php
@@ -55,25 +55,25 @@ $map = [
     <meta name="twitter:creator" content="<?php echo $twitter_creator; ?>">
     <meta name="twitter:title" content="<?php echo $twitter_title; ?>">
     <meta name="twitter:description" content="<?php echo $twitter_description; ?>">
-    <meta name="twitter:image" content="./assets/icons/ms-icon-310x310.png">
-    <link rel="apple-touch-icon" sizes="./assets/icons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="./assets/icons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="./assets/icons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/icons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="./assets/icons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="./assets/icons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="./assets/icons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="./assets/icons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="./assets/icons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon-16x16.png">
+    <meta name="twitter:image" content="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $map[$r]['gif']; ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $map[$r]['gif']; ?>">
     <link rel="manifest" href="./assets/icons/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-144x144.png">
-    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-150x150.png">
-    <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-310x310.png">
+    <meta name="msapplication-TileImage" content="<?php echo $map[$r]['gif']; ?>">
+    <meta name="msapplication-TileImage" content="<?php echo $map[$r]['gif']; ?>">
+    <meta name="msapplication-TileImage" content="<?php echo $map[$r]['gif']; ?>">
     <meta name="theme-color" content="#ffffff">
     <title><?php echo $title; ?></title>
 
